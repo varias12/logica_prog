00f8f109f8e34234cc4dcaebd320dfa09865734f
varias12 <101846330+varias12@users.noreply.github.com>
2022-03-24 19:32:42 -0600

    Par o Impar

---
+++ Tarea_1.4.py
@@ -0,0 +1,21 @@
+#Tarea 1
+#Ejercicio 4
+
+
+import math
+
+#Entrada de datos
+
+número = int(input("Pon un número:"))
+
+#Proceso
+
+if número % 2 == 0:
+
+    print("El número es par")
+
+else: 
+    
+    print("El número es impar")
+
+
