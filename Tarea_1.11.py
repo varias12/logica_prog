e85b4314eccd2335ce0e50e2e1902332626ccef2
varias12 <101846330+varias12@users.noreply.github.com>
2022-03-31 17:54:33 -0600

    Función que trae arreglo o lista

---
+++ Tarea_1.11.py
@@ -0,0 +1,30 @@
+#Tarea 11
+#Ejercicio 11
+#Función que imprima un arreglo o lista de 10 elementos
+
+arreglo_de_números_desordenado = [4,3,8,9,24,7,1,12,10,11]
+
+arreglo_de_números_desordenado.sort()
+
+def Ordenar_arreglo(arreglo_de_números_desordenado):
+    ordenar = arreglo_de_números_desordenado
+    return ordenar
+
+o = Ordenar_arreglo(arreglo_de_números_desordenado)
+
+print(f"El arreglo ordenado queda así:{o}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
